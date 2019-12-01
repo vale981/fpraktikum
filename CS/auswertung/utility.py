013fb3294a57b2e78a68c345c354d9a16a67f876
hiro98 <hiro@protagon.space>
2019-12-01 17:29:00 +0100

    crossec discussion

--- CS/auswertung/utility.py
+++ CS/auswertung/utility.py
@@ -154,7 +154,7 @@ def save_fig(fig, title, folder='unsorted', size=(5, 4)):
   + f'{folder}/{title}.pgf' +
   r'''}
   \caption{}
-  \label{fig:}
+  \label{fig:''' + title + '''}
 \end{figure}
     ''')
 
